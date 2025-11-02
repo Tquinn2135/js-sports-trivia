1f99f8082704c56153b36bc367d215ae3a0c7fa9
TQuinn2135 <Tquinn2135@gmail.com>
2025-11-01 20:47:43 -0500

    TPQ.Exam2 finished

--- site.js
+++ site.js
@@ -11,11 +11,48 @@ const nextQuestionElement = document.querySelector('#nextQuestion')
 ;(async () => {
 
 	// todo: create your "getNextQuestion" function
+	const getNextQuestion = async () => {
+		const url = 'https://opentdb.com/api.php?amount=1&category=21&difficulty=easy&type=multiple'
+		const res = await fetch(url)
+		const json = await res.json()
+		const { question, correct_answer: correct, incorrect_answers: incorrect } = json.results[0]
+		const answers = shuffle([...incorrect, correct ])		
+		return{ question, answers, correct }		
+	}
+	//console.log(await getNextQuestion())
 
 	// todo: create your "renderQuestion" function
+	const renderQuestion = async ({ question, answers, correct }) => {
+		answersElement.textContent = decodeHtml(question)
 
+		questionElement.innerHTML = ''
+
+		answers.forEach(answer => {
+			const button = document.createElement('button')
+			button.classList.add('answer')
+			button.innerHTML = decodeHtml(answer)
+			button.addEventListener('click', () => {
+				if( answer=== correct){
+				button.classList.add('correct')
+				answersElement.querySelectorAll('button').forEach(b => b.disabled = true)
+				alert('Correct!')
+				}
+				else{
+					button.disabled = true
+					alert('Wrong answer!')
+				}				
+			})			
+			answersElement.appendChild(button)			
+	})
+}
 	// todo: add the event listener to the "nextQuestion" button
+	nextQuestionElement.addEventListener('click', async() =>{		
+		renderQuestion(await getNextQuestion())
+	})
 
+	renderQuestion(await getNextQuestion())
+	nextQuestionElement.disabled = true
+	setTimeout(() => nextQuestionElement.disabled = false, 10000)
 })()
 
 // mimic a click on the "nextQuestion" button to show the first question
